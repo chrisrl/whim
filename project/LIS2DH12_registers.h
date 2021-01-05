82315ad42aa9c33ac0440d8fb07cee3739f0b7a4
cshaul <32314502+cshaul@users.noreply.github.com>
2021-01-04 20:49:00 -0700

    Code formatting & twos compliment data correction

--- project/LIS2DH12_registers.h
+++ project/LIS2DH12_registers.h
@@ -16,92 +16,97 @@ accelerometer module
 															     MACROS
 *******************************************************************************/
 
+// Any additions to this type def must be at the bottom in order to preserve the memory addresses of the registers defined in the enum below
 typedef struct {
 	uint8_t reserved0[7];
-	uint8_t STATUS_REG_AUX;
+	uint8_t status_reg_aux;
 	uint8_t reserved1[4];
-	uint8_t OUT_TEMP_L;
-	uint8_t OUT_TEMP_H;
+	uint8_t out_temp_l;
+	uint8_t out_temp_h;
 	uint8_t reserved2;
-	uint8_t WHO_AM_I;
+	uint8_t who_am_i;
 	uint8_t reserved3[14];
-	uint8_t CTRL_REG0;
-	uint8_t TEMP_CFG_REG;
-	uint8_t CTRL_REG1;
-	uint8_t CTRL_REG2;
-	uint8_t CTRL_REG3;
-	uint8_t CTRL_REG4;
-	uint8_t CTRL_REG5;
-	uint8_t CTRL_REG6;
-	uint8_t REFERENCE;
-	uint8_t STATUS_REG2;
-	uint8_t OUT_X_L;
-	uint8_t OUT_X_H;
-	uint8_t OUT_Y_L;
-	uint8_t OUT_Y_H;
-	uint8_t OUT_Z_L;
-	uint8_t OUT_Z_H;
-	uint8_t FIFO_CTRL_REG;
-	uint8_t FIFO_SRC_REG;
-	uint8_t INT1_CFG;
-	uint8_t INT1_SOURCE;
-	uint8_t INT1_THS;
-	uint8_t INT1_DURATION;
-	uint8_t INT2_CFG;
-	uint8_t INT2_SOURCE;
-	uint8_t INT2_THS;
-	uint8_t INT2_DURATION;
-	uint8_t CLICK_CFG;
-	uint8_t CLICK_SRC;
-	uint8_t CLICK_THS;
-	uint8_t TIME_LIMIT;
-	uint8_t TIME_LATENCY;
-	uint8_t TIME_WINDOW;
-	uint8_t Act_THS;
-	uint8_t Act_DUR;
-} LIS2DH12_REGISTER_MAP;
+	uint8_t ctrl0;
+	uint8_t temp_cfg;
+	uint8_t ctrl1;
+	uint8_t ctrl2;
+	uint8_t ctrl3;
+	uint8_t ctrl4;
+	uint8_t ctrl5;
+	uint8_t ctrl6;
+	uint8_t reference;
+	uint8_t status_reg2;
+	uint8_t out_x_l;
+	uint8_t out_x_h;
+	uint8_t out_y_l;
+	uint8_t out_y_h;
+	uint8_t out_z_l;
+	uint8_t out_z_h;
+	uint8_t fifo_ctrl;
+	uint8_t fifo_src;
+	uint8_t int1_cfg;
+	uint8_t int1_src;
+	uint8_t int1_ths;
+	uint8_t int1_dur;
+	uint8_t int2_cfg;
+	uint8_t int2_src;
+	uint8_t int2_ths;
+	uint8_t int2_dur;
+	uint8_t click_cfg;
+	uint8_t click_src;
+	uint8_t click_ths;
+	uint8_t time_limit;
+	uint8_t time_latency;
+	uint8_t time_window;
+	uint8_t act_ths;
+	uint8_t act_dur;
+	
+	///////////////////////// Add any non-register struct members below this point
+	uint8_t full_scale_value;
+	uint8_t resolution;
+} lis2dh12_instance_t;
 
-STATIC_ASSERT(sizeof(LIS2DH12_REGISTER_MAP) != 0x3F);
+STATIC_ASSERT(sizeof(lis2dh12_instance_t) != 0x3F);
 
 enum {
-	STATUS_REG_AUX = offsetof(LIS2DH12_REGISTER_MAP, STATUS_REG_AUX),
-	OUT_TEMP_L = offsetof(LIS2DH12_REGISTER_MAP, OUT_TEMP_L),
-	OUT_TEMP_H = offsetof(LIS2DH12_REGISTER_MAP, OUT_TEMP_H),
-	WHO_AM_I = offsetof(LIS2DH12_REGISTER_MAP, WHO_AM_I),
-	CTRL_REG0 = offsetof(LIS2DH12_REGISTER_MAP, CTRL_REG0),
-	TEMP_CFG_REG = offsetof(LIS2DH12_REGISTER_MAP, TEMP_CFG_REG),
-	CTRL_REG1 = offsetof(LIS2DH12_REGISTER_MAP, CTRL_REG1),
-	CTRL_REG2 = offsetof(LIS2DH12_REGISTER_MAP, CTRL_REG2),
-	CTRL_REG3 = offsetof(LIS2DH12_REGISTER_MAP, CTRL_REG3),
-	CTRL_REG4 = offsetof(LIS2DH12_REGISTER_MAP, CTRL_REG4),
-	CTRL_REG5 = offsetof(LIS2DH12_REGISTER_MAP, CTRL_REG5),
-	CTRL_REG6 = offsetof(LIS2DH12_REGISTER_MAP, CTRL_REG6),
-	REFERENCE = offsetof(LIS2DH12_REGISTER_MAP, REFERENCE),
-	STATUS_REG2 = offsetof(LIS2DH12_REGISTER_MAP, STATUS_REG2),
-	OUT_X_L = offsetof(LIS2DH12_REGISTER_MAP, OUT_X_L),
-	OUT_X_H = offsetof(LIS2DH12_REGISTER_MAP, OUT_X_H),
-	OUT_Y_L = offsetof(LIS2DH12_REGISTER_MAP, OUT_Y_L),
-	OUT_Y_H = offsetof(LIS2DH12_REGISTER_MAP, OUT_Y_H),
-	OUT_Z_L = offsetof(LIS2DH12_REGISTER_MAP, OUT_Z_L),
-	OUT_Z_H = offsetof(LIS2DH12_REGISTER_MAP, OUT_Z_H),
-	FIFO_CTRL_REG = offsetof(LIS2DH12_REGISTER_MAP, FIFO_CTRL_REG),
-	FIFO_SRC_REG = offsetof(LIS2DH12_REGISTER_MAP, FIFO_SRC_REG),
-	INT1_CFG = offsetof(LIS2DH12_REGISTER_MAP, INT1_CFG),
-	INT1_SOURCE = offsetof(LIS2DH12_REGISTER_MAP, INT1_SOURCE),
-	INT1_THS = offsetof(LIS2DH12_REGISTER_MAP, INT1_THS),
-	INT1_DURATION = offsetof(LIS2DH12_REGISTER_MAP, INT1_DURATION),
-	INT2_CFG = offsetof(LIS2DH12_REGISTER_MAP, INT2_CFG),
-	INT2_SOURCE = offsetof(LIS2DH12_REGISTER_MAP, INT2_SOURCE),
-	INT2_THS = offsetof(LIS2DH12_REGISTER_MAP, INT2_THS),
-	INT2_DURATION = offsetof(LIS2DH12_REGISTER_MAP, INT2_DURATION),
-	CLICK_CFG = offsetof(LIS2DH12_REGISTER_MAP, CLICK_CFG),
-	CLICK_SRC = offsetof(LIS2DH12_REGISTER_MAP, CLICK_SRC),
-	CLICK_THS = offsetof(LIS2DH12_REGISTER_MAP, CLICK_THS),
-	TIME_LIMIT = offsetof(LIS2DH12_REGISTER_MAP, TIME_LIMIT),
-	TIME_LATENCY = offsetof(LIS2DH12_REGISTER_MAP, TIME_LATENCY),
-	TIME_WINDOW = offsetof(LIS2DH12_REGISTER_MAP, TIME_WINDOW),
-	Act_THS = offsetof(LIS2DH12_REGISTER_MAP, Act_THS),
-	Act_DUR = offsetof(LIS2DH12_REGISTER_MAP, Act_DUR),
+	STATUS_REG_AUX = offsetof(lis2dh12_instance_t, status_reg_aux),
+	OUT_TEMP_L = offsetof(lis2dh12_instance_t, out_temp_l),
+	OUT_TEMP_H = offsetof(lis2dh12_instance_t, out_temp_h),
+	WHO_AM_I = offsetof(lis2dh12_instance_t, who_am_i),
+	CTRL_REG0 = offsetof(lis2dh12_instance_t, ctrl0),
+	TEMP_CFG_REG = offsetof(lis2dh12_instance_t, temp_cfg),
+	CTRL_REG1 = offsetof(lis2dh12_instance_t, ctrl1),
+	CTRL_REG2 = offsetof(lis2dh12_instance_t, ctrl2),
+	CTRL_REG3 = offsetof(lis2dh12_instance_t, ctrl3),
+	CTRL_REG4 = offsetof(lis2dh12_instance_t, ctrl4),
+	CTRL_REG5 = offsetof(lis2dh12_instance_t, ctrl5),
+	CTRL_REG6 = offsetof(lis2dh12_instance_t, ctrl6),
+	REFERENCE = offsetof(lis2dh12_instance_t, reference),
+	STATUS_REG2 = offsetof(lis2dh12_instance_t, status_reg2),
+	OUT_X_L = offsetof(lis2dh12_instance_t, out_x_l),
+	OUT_X_H = offsetof(lis2dh12_instance_t, out_x_h),
+	OUT_Y_L = offsetof(lis2dh12_instance_t, out_y_l),
+	OUT_Y_H = offsetof(lis2dh12_instance_t, out_y_h),
+	OUT_Z_L = offsetof(lis2dh12_instance_t, out_z_l),
+	OUT_Z_H = offsetof(lis2dh12_instance_t, out_z_h),
+	FIFO_CTRL_REG = offsetof(lis2dh12_instance_t, fifo_ctrl),
+	FIFO_SRC_REG = offsetof(lis2dh12_instance_t, fifo_src),
+	INT1_CFG = offsetof(lis2dh12_instance_t, int1_cfg),
+	INT1_SOURCE = offsetof(lis2dh12_instance_t, int1_src),
+	INT1_THS = offsetof(lis2dh12_instance_t, int1_ths),
+	INT1_DURATION = offsetof(lis2dh12_instance_t, int1_dur),
+	INT2_CFG = offsetof(lis2dh12_instance_t, int2_cfg),
+	INT2_SOURCE = offsetof(lis2dh12_instance_t, int2_src),
+	INT2_THS = offsetof(lis2dh12_instance_t, int2_ths),
+	INT2_DURATION = offsetof(lis2dh12_instance_t, int2_dur),
+	CLICK_CFG = offsetof(lis2dh12_instance_t, click_cfg),
+	CLICK_SRC = offsetof(lis2dh12_instance_t, click_src),
+	CLICK_THS = offsetof(lis2dh12_instance_t, click_ths),
+	TIME_LIMIT = offsetof(lis2dh12_instance_t, time_limit),
+	TIME_LATENCY = offsetof(lis2dh12_instance_t, time_latency),
+	TIME_WINDOW = offsetof(lis2dh12_instance_t, time_window),
+	ACT_THS = offsetof(lis2dh12_instance_t, act_ths),
+	ACT_DUR = offsetof(lis2dh12_instance_t, act_dur),
 };
 
 #define CTRL_REG0_VALID_MASK (uint8_t)(0x10)
