1f986a101de14d71eaa3005f4c2bece597d02c28
chrisrl <lolargac@gmail.com>
2021-01-07 17:09:15 -0700

    Add probe, pwrdn, enable, disable and format

--- project/LIS2DH12.h
+++ project/LIS2DH12.h
@@ -23,30 +23,43 @@ and communication with the LIS2DH12 accelerometer module
 //#define SPI_DEBUG_INFO
 //#define ACCEL_DEBUG_INFO
 
-#define SCALE_SELECT_MASK ((uint8_t) 0x30)
-#define SCALE_2G ((uint8_t) 2)
-#define SCALE_4G ((uint8_t) 4)
-#define SCALE_8G ((uint8_t) 8)
-#define SCALE_16G ((uint8_t) 16)
+#define SCALE_SELECT_MASK (CTRL_REG4_FS0 | CTRL_REG4_FS1)
 
 #define OP_MODE_MASK 0x08
-#define HIGH_RES_BITS 12
-#define NORMAL_RES_BITS 10
-#define LOW_RES_BITS 8
 #define SENSITIVITY ((float) 0.048) //We can change this to make it dyanmically assigned based off of the FS bit settings/op mode
 
-#define XYZ_REG_SIZE 16
+#define XYZ_REG_SIZE sizeof(uint16_t)
 #define MAGNITUDE_MASK 0x0FFF
+
+#define LIS2DH12_DEVICE_ID 33
 
 /*******************************************************************************
 															     TYPEDEFS
 *******************************************************************************/
+typedef struct {
+	lis2dh12_reg_map_t reg_map;
+	uint8_t full_scale_value;
+	uint8_t resolution;
+} lis2dh12_instance_t;
 
 typedef struct {
 	float out_x;
 	float out_y;
 	float out_z;
 } accel_xyz_data_t;
+
+enum {
+	SCALE_2G = 2,
+	SCALE_4G = 4,
+	SCALE_8G = 8,
+	SCALE_16G = 16,
+};
+
+enum {
+	HIGH_RES_BITS = 12,
+	NORMAL_RES_BITS = 10,
+	LOW_RES_BITS = 8
+};
 
 enum {
 	MAX_2G = 0x00,
@@ -66,11 +79,9 @@ enum {
 *******************************************************************************/
 
 void ACCEL_read_xyz(accel_xyz_data_t* xyz_data, lis2dh12_instance_t* accel_inst);
+void ACCEL_disable_fifo(void);
+bool ACCEL_enable_fifo(void);
 bool ACCEL_init(lis2dh12_instance_t* accel_inst);
-void accel_gpio_init(void);
-void accel_wtm_event_handler(nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t polarity);
-void accel_fifo_check(void);
-void accel_fifo_reset(void);
-
+void ACCEL_pwrdn(void);
 
 #endif /*LIS2DH12_H*/
